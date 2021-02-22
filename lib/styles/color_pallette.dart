321d9a9fbc5482bcee4b5e9e500b899f7b44f047
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-02-22 13:04:21 +0545

    first commit added theme styles inside main.dart added text themes

---
+++ lib/styles/color_pallette.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color colorPrimary = Color(0xFF439C7C);
+  static const Color colorPrimaryDim = Color(0xFFDEF2EE);
+  static const Color colorSecondary = Color(0xFF174E4D);
+
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+
+  static const primaryLightGreen = Color(0xFFE5F8F4);
+  static const primaryGreen = Color(0xFF429A7A);
+  static const secondaryGreen = Color(0xFF26685D);
+
+  static const errorRed = Color(0xFFFF5451);
+  static const errorLightRed = Color(0xFFFFDCDB);
+
+  static const warningYellow= Color(0xFFFEBE4E);
+  static const warningLightYellow = Color(0xFFFFF3DD);
+
+  static const neutralGrey = Color(0xFF707070);
+  static const neutralLightGrey= Color (0xFFEBECEF);
+
+  static const secondaryTextBlue = Color(0xFF555B75);
+
+  static const primaryTextGrey = neutralGrey;
+  static const scaffoldColor = neutralLightGrey;
+}
---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+import 'color_pallette.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles{
+  static const _base = TextStyle(
+    color: Palette.black,
+    // fontFamily: AppConfig.fontFamily,
+  );
+
+  static final headline1 = _base.copyWith(
+    fontSize: 32.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final headline2 = _base.copyWith(
+    fontSize: 26.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final headline3 = _base.copyWith(
+    fontSize: 22.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final headline4 = _base.copyWith(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final headline5 = _base.copyWith(
+    fontSize: 15.0,
+    color: Palette.colorPrimary,
+    fontWeight: FontWeight.w300,
+  );
+
+  static final headline6 = _base.copyWith(
+    fontSize: 11.0,
+    color: Palette.darkFaded,
+    fontWeight: FontWeight.w200,
+  );
+}
